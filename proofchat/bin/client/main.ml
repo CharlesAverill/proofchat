50169362f7257c2fd2e5d217b7c2cc6b5c1aeee0
Charles Averill <charlesaverill20@gmail.com>
2024-09-25 15:30:44 -0500

    Add address and port options to server and client

--- proofchat/bin/client/main.ml
+++ proofchat/bin/client/main.ml
@@ -1,7 +1,22 @@
 open Client
 open Proofchat.Logging
 
+let address = ref "127.0.0.1"  (* Default address *)
+let port = ref 8888            (* Default port *)
+
+(* Parse command-line arguments *)
+let speclist = [
+  ("-a", Arg.Set_string address, "Address to connect to (default: 127.0.0.1)");
+  ("-p", Arg.Set_int port, "Port to connect to (default: 8888)");
+]
+
+let usage_msg = "Usage: dune exec -- proofchat.client [-a address] [-p port]"
+
 let () =
-  match client "127.0.0.1" 8888 with
+  (* Parse the arguments based on the speclist *)
+  Arg.parse speclist (fun _ -> ()) usage_msg;
+
+  (* Attempt to connect using the provided or default address/port *)
+  match client !address !port with
   | NoneE err -> fatal rc_Error err
   | _ -> ()
--- proofchat/bin/server/main.ml
+++ proofchat/bin/server/main.ml
@@ -1,7 +1,22 @@
 open Server
 open Proofchat.Logging
 
+let address = ref "0.0.0.0"  (* Default address *)
+let port = ref 8888            (* Default port *)
+
+(* Parse command-line arguments *)
+let speclist = [
+  ("-a", Arg.Set_string address, "Address to connect to (default: 127.0.0.1)");
+  ("-p", Arg.Set_int port, "Port to connect to (default: 8888)");
+]
+
+let usage_msg = "Usage: dune exec -- proofchat.server [-a address] [-p port]"
+
 let () =
-  match server "127.0.0.1" 8888 with
+  (* Parse the arguments based on the speclist *)
+  Arg.parse speclist (fun _ -> ()) usage_msg;
+
+  (* Attempt to connect using the provided or default address/port *)
+  match server !address !port with
   | NoneE err -> fatal rc_Error err
   | _ -> ()
